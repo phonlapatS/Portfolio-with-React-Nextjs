d57118ca49f6ac1bf292c6e50f648a159a86368e
Rocket <PhonlapatS.2002@gmail.com>
2024-07-14 16:40:40 +0700

    Add Components folder , Edited header.tsx and add Header to layout.tsx (Make it client) add third commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { Header } from '@/components/header'
 import './globals.css'
 import { Inter } from 'next/font/google'
 
@@ -15,12 +16,15 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-slate-200 text-gray-950`}> {/* Background Color */}
+      <body className={`${inter.className} bg-slate-200 text-gray-950
+      relative h-[5000px]`}> {/* Background Color */}
           <div className='bg-[#e2fbe6] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]'></div>{/* right green circle */}
           <div className='bg-[#dbd7fd] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]
           md:left[-33rem] lg:left-[-28rem] xl:left=[-15rem] 2xl:left-[-5rem]'></div>{/* left blue circle */}
+
+         <Header/>
         {children}
         </body>
     </html>
-  )
+  );
 }
