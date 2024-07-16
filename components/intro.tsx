e3d1cc3f51905e9b64ab101a9bf50d2aa0c0fae1
Phonlapat Suanghiran <Roocket@admins-iMac-2.local>
2024-07-16 14:46:18 +0700

    Fixed wrong folder - 6.2 commit

--- components/intro.tsx
+++ components/intro.tsx
@@ -53,7 +53,7 @@ export default function Intro() {
             </div>
         </div>
 
-        <motion.p className='mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] 
+        <motion.h1 className='mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] 
         sm:text-4xl'
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
@@ -63,7 +63,7 @@ export default function Intro() {
           <span className="font-bold">1 years</span> of experience. I enjoy
           building <span className="italic">sites & apps</span>. My focus is{" "}
           <span className="underline">React (Next.js), MongoDB and Go </span>.
-        </motion.p>
+        </motion.h1>
         
                   <motion.div className='flex flex-col sm:flex-row items-center justify-center
                     gap-2 px-4 text-lg font-medium '
