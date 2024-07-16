b20d6dc986a977e383cb1515cd434935140e3c64
Phonlapat Suanghiran <Roocket@admins-iMac-2.local>
2024-07-16 16:32:19 +0700

    Add Project section and add projects to interact each other - 7 commit

---
+++ components/projects.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import React from 'react';
+import { projectsData } from '@/lib/data';
+import SectionHeading from './section-heading';
+import Project from './project';
+
+export default function Projects() {
+  return (
+  <section>
+    <SectionHeading> My projects</SectionHeading>
+    <div>
+        {projectsData.map((project, index) => (
+                <React.Fragment key={index}>
+                    <Project {...project}/>
+                </React.Fragment>
+            ))}
+    </div>
+
+  </section>
+  )
+}
+
