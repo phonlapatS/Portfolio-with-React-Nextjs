b9db94a9b1b65d1ab5ba050c3b7aded1719311f4
Rocket <PhonlapatS.2002@gmail.com>
2024-07-17 02:17:38 +0700

    Add animate to header menu and add Context to interact with menu and components

--- components/header.tsx
+++ components/header.tsx
@@ -4,8 +4,13 @@ import React from 'react'
 import { motion } from "framer-motion";
 import { links } from '@/lib/data';
 import Link from 'next/link';
+import clsx from 'clsx';
+import { useActiveSectionContext } from '@/context/active-section-context';
+
 
 export function Header() {
+   const { activeSection, setActiveSection } = useActiveSectionContext();
+
   return (
   <header className='z-[999] relative'>
     <motion.div className='fixed top-0 left-1/2 -translate-x-1/2 h-[4.5rem] w-full rounded-none 
@@ -25,12 +30,31 @@ export function Header() {
             {links.map(link => (
                 <motion.li 
                     className='h-3/4 flex items-center 
-                    justify-center' key={link.hash}
+                    justify-center relative' key={link.hash}
                     initial={{ y: -100, opacity: 0}}
                     animate={{ y: 0, opacity: 1 }}
                 >
-                    <Link className='flex w-full items-center justify-center
-                    px-3 py-3 hover:text-gray-950' href={link.hash}>{link.name}
+                    <Link className={clsx(
+                        "flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition", {
+                            "text-gray-950": activeSection === link.name,
+                        })}
+                    href={link.hash}
+                    onClick={() => setActiveSection(link.name)}
+                    >
+                        {link.name}
+                    {
+                        link.name === activeSection && (
+                            <motion.span className='bg-gray-100 rounded-full absolute inset-0 -z-10'
+                            layoutId="activeSection"
+                            transition={{
+                                type: "spring",
+                                stiffness: 380,
+                                damping: 30
+                            }}
+                            ></motion.span>
+
+                        )}
+                    
                     </Link>
                 </motion.li>
             ))}
